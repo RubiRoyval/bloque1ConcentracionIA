8ffed4e2899077c7e9130a47249560c4969e31c3
RubiRoyval <93799603+RubiRoyval@users.noreply.github.com>
2024-08-27 19:18:46 -0600

    Error 2

--- serverML.py
+++ serverML.py
@@ -25,7 +25,7 @@ def predictjson():
     #Predecir utilizando la entrada y el modelo 
     result= dt.predictjson(inputData.reshape(1,-1))
     #Enviar respuesta
-    return jsonify({'Prediction'_str(result[0])})
+    return jsonify({'Prediction':str(result[0])})
 
 if __name__ == '__main__':
     server.run(debug=False,host='0.0.0.0',port=8080)
