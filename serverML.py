4562213593aba8ecd4af46b4e4050c8d7b916eb9
RubiRoyval <93799603+RubiRoyval@users.noreply.github.com>
2024-08-27 19:43:44 -0600

    Cambios

--- serverML.py
+++ serverML.py
@@ -7,7 +7,7 @@ from werkzeug.utils import secure_filename
 #Load model
 dt = joblib.load('dtl-2.joblib')
 #Create Flask App
-server = Flask(_name_)
+server = Flask(__name__)
 #Define route
 @server.route('/predictjson', methods=['POST'])
 def predictjson():
@@ -20,7 +20,7 @@ def predictjson():
     ])
     result= dt.predict(inputData.reshape(1,-1))
     return jsonify({"Prediction": str(result[0])})
-if _name_ == "_main_":
+if __name__ == "__main__":
     server.run(debug= False , host ="0.0.0.0",port=8080)
 
 
